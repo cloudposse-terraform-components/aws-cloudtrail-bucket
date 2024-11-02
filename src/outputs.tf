8ce7fe486f45eac3d56c6e5d6074dae8928a59e7
Igor Rodionov <goruha@gmail.com>
2024-11-02 19:17:05 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,14 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "cloudtrail_bucket_domain_name" {
+  value       = module.cloudtrail_s3_bucket.bucket_domain_name
+  description = "CloudTrail S3 bucket domain name"
+}
+
+output "cloudtrail_bucket_id" {
+  value       = module.cloudtrail_s3_bucket.bucket_id
+  description = "CloudTrail S3 bucket ID"
+}
+
+output "cloudtrail_bucket_arn" {
+  value       = module.cloudtrail_s3_bucket.bucket_arn
+  description = "CloudTrail S3 bucket ARN"
 }
