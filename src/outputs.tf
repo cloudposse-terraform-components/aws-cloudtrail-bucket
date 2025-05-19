3970ec2385e22617abb0e2ab1baa9a1ffc227147
RoseSecurity <michaeljrosenfeld22@gmail.com>
2025-05-19 14:12:38 -0400

    feat: parameterize input values and add null handling to outputs

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,14 +1,14 @@
 output "cloudtrail_bucket_domain_name" {
-  value       = module.cloudtrail_s3_bucket.bucket_domain_name
+  value       = try(module.cloudtrail_s3_bucket.bucket_domain_name, null)
   description = "CloudTrail S3 bucket domain name"
 }
 
 output "cloudtrail_bucket_id" {
-  value       = module.cloudtrail_s3_bucket.bucket_id
+  value       = try(module.cloudtrail_s3_bucket.bucket_id, null)
   description = "CloudTrail S3 bucket ID"
 }
 
 output "cloudtrail_bucket_arn" {
-  value       = module.cloudtrail_s3_bucket.bucket_arn
+  value       = try(module.cloudtrail_s3_bucket.bucket_arn, null)
   description = "CloudTrail S3 bucket ARN"
 }
